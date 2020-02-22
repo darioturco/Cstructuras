10f7473b76768b47966a501e5c8c1f91d00c51ce
Dario <diort.2018@gmail.com>
2020-02-22 00:24:50 -0300

    Defini las funciones de Disjoint-sets

---
+++ Disjoint.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+template <typename T>
+
+class Disjoint{
+	private:
+	struct set{
+		T head;
+		T* childs = [];
+		//Usar listas simplemente enlasadas
+	};
+	
+	struct element* head = NULL;
+	
+	public:
+	int lenght = 0;// cuenta la cantidad de sets
+	
+	Disjoint(){
+		
+	}
+	
+	//void addSet(){}
+	
+	//void removeSet(int index){}
+	
+	//void join(int set1, int set2){}
+	
+	//bool areJoined(int set1, int set2){}
+	
+	//int findSet(T obj){}
+	
+	//bool isEmpty(){}
+	
+	//void clear(){}
+	
+	//void printSet(int index){}
+	
+	//void print(){}
+	
+};
